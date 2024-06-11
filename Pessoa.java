331627d11c6cfd82aeb286d0735ae1aa849e2834
Erivelton Arantes de Souza <162745958+EriveltonArantes@users.noreply.github.com>
2024-06-10 22:10:26 -0300

    Update Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -16,9 +16,15 @@ class Pessoa {
         return nome;
     }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
     public LocalDate getDataNascimento() {
         return dataNascimento;
     }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
 }
-
-
